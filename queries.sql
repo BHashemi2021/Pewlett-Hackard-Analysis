a596c004bb610f6f7bdde497af5f482836b36caa
BHashemi2021 <bhashemi2021@gmail.com>
2021-06-09 18:18:07 -0400

    updates to queries.sql file.

--- queries.sql
+++ queries.sql
@@ -1,105 +1,74 @@
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
-);
 
-
-CREATE TABLE employees (
-    emp_no INT NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    gender VARCHAR NOT NULL,
-    hire_date DATE NOT NULL,
-    PRIMARY KEY (emp_no)
-);
-
-
-CREATE TABLE dept_manager (
-    dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
-
-
-CREATE TABLE salaries (
-    emp_no INT NOT NULL,
-    salary INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
-);
-
-
-CREATE TABLE Dept_Emp (
-	dept_no VARCHAR NOT NULL,
-	emp_no VARCHAR NOT NULL,
-	from_date date NOT NULL,
-	to_date date NOT NULL
-);
-
-
-CREATE TABLE Titles (
-	emp_no int,
-	title VARCHAR,
-	from_date date,
-	to_date date,
-	PRIMARY KEY (emp_no, title, from_date)
-); 
-
-
+-- QUESRIES START FROM HERE
 
 -- finding the name of employees born between 1952 and 1955 who will begin to retire
 SELECT first_name, last_name
 FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+;
 
 
--- Create another query that will search for only 1952 birth dates.
+
+
+
+-- Create another query that will search for only 1952 birth dates.	
 SELECT first_name, last_name
 FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'
+;
 
 
--- Retirement eligibility
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
 
 
--- Finding the retirement eligibility and those hired between 1985 to 1988
+
+
+-- Finding the retirement eligibility and those hired between 1985 to 1988 
 SELECT first_name, last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+;
 
 
--- Counting the number of employees retiring
+
+
+
+-- Number of employees retiring
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+;
+
+
+
+
 
 -- Using SELECT INTO to save data as a new table INTO retirement_info
 SELECT first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+;
 
 
--- To  see what the retirement_info​ table looks like we query it with a SELECT statement	
-SELECT * FROM retirement_info;
 
 
--- By Right-clicking on retirement_info table we save the data in the Queries folder
+
+
+-- To  see what the retirement_info​ table looks like we query it with a SELECT statement 
+SELECT * FROM retirement_info
+;
+
+-- To see how many records are there in thid table
+SELECT COUNT (first_name)
+FROM retirement_info
+;
+
+
+
+
+
 
 
